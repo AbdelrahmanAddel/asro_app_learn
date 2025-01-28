71948eb44861382c85a70a8549c5358c5f76a39b
Abdelrahman Adel <aa5282243@gmail.com>
2025-01-28 18:06:34 +0200

    first commit

---
+++ lib/asroo_app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AsrooApp extends StatelessWidget {
+  const AsrooApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Placeholder(),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:asroo_store/asroo_app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const AsrooApp());
+}
+
